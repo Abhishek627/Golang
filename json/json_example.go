a03188b0cef0185988ab0e0406bc06ce33f0ea9d
shabhishek <shabhishek@juniper.net>
2019-06-16 21:29:26 +0530

    Added json marshal and unmarshal

---
+++ json/json_example.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Employee struct {
+	Name string `json:name`
+	Salary int `json:salary`
+}
+
+func MustMarshal(data interface{}) []byte{
+	out,err:= json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+func main(){
+
+	var emp Employee
+	data := []byte (`
+		{
+			"name":"Abhishek Sharma",
+			"salary":10000
+		}
+		`)
+
+	err := json.Unmarshal(data, &emp)
+	if err == nil{
+		fmt.Print(emp.Name)
+	}
+
+	mapD := map[string]int{"nameid":1, "salary": 7}
+    mapB:= MustMarshal(mapD)
+    fmt.Println(string(mapB))
+
+}
